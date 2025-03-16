bbd732aad1002ba65c6cfda8cdd8c934f22c2858
Khalil Laouini <laouinikhalil@gmail.com>
2025-03-16 21:51:36 +0100

    First commit

--- src/main/java/com/pi/restful_library/ResTfulLibraryApplication.java
+++ src/main/java/com/pi/restful_library/ResTfulLibraryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ResTfulLibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResTfulLibraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulLibraryApplication.class, args);
+    }
 
 }
